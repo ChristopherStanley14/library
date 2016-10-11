ca065e5deae9478e962560f4535d6008db47c15e
ChristopherStanley14 <christopherstanley@ChristopherStanleyMBP.local>
2016-10-11 10:55:44 -0400

    first commit

---
+++ library-ccs/library-ccs/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  library-ccs
+//
+//  Created by Christopher Stanley on 10/11/16.
+//  Copyright © 2016 Christopher Stanley. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
